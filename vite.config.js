ff250dc3d7c13a7d3f13858e009e09b5e24d51b1
santiagoavs <santiago4v5@gmail.com>
2025-09-28 22:43:09 -0600

    Versión 1.5.1 Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
+  base: './', // Use relative paths for production
   plugins: [react()],
   server: {
     proxy: {
@@ -11,5 +12,16 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: mode === 'development',
+    minify: mode === 'production' ? 'terser' : false,
+    chunkSizeWarningLimit: 1600,
+  },
+  define: {
+    'process.env': process.env,
+    // Add any other environment variables here
   }
-})
+}))
